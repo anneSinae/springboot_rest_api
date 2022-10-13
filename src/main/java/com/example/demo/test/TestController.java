6cd9f0f23f20967cbcefd955098f9ce10b2a93b8
KDN User <User@KDN_PC.kdn.com>
2022-10-13 14:33:34 +0900

    test requestMapping

--- src/main/java/com/example/demo/test/TestController.java
+++ src/main/java/com/example/demo/test/TestController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.User;
 
 @RestController
+@RequestMapping("info")
 public class TestController {
 	
 	private TestService testService;
@@ -20,20 +22,20 @@ public class TestController {
 		this.testService = testService;
 	}
 	
-	@GetMapping("/info")
+	@GetMapping("")
 	public Object testInfo() {
 		//User users = testService.getUsersInfo();
 		List<User> userList = testService.getUserList();
 		return userList;
 	}
 	
-	@GetMapping("/info/param/{name}") //http://localhost:8080/info/param/원펀맨
+	@GetMapping("param/{name}") //http://localhost:8080/info/param/원펀맨
 	public Object testInfoByPathVariable(@PathVariable("name") String name) {
 		List<User> userList = testService.getUserListByName(name);
 		return userList;
 	}
 	
-	@GetMapping("/info/param") //http://localhost:8080/info/param?name=원펀맨
+	@GetMapping("param") //http://localhost:8080/info/param?name=원펀맨
 	public Object testInfoRequestParam(@RequestParam(value = "name", required = false, defaultValue = "홍길동") String name) {
 		List<User> userList = testService.getUserListByName(name);
 		return userList;
