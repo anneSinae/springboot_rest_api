2d8fa9dcbdefec388bc9ac577fc9182e0c054278
KDN User <User@KDN_PC.kdn.com>
2022-10-14 15:58:37 +0900

    connect DB data on layout content file

--- src/main/java/com/example/demo/board/BoardController.java
+++ src/main/java/com/example/demo/board/BoardController.java
@@ -1,20 +1,35 @@
 package com.example.demo.board;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.model.User;
+import com.example.demo.test.TestService;
 
 @RestController
 @RequestMapping("board")
 public class BoardController {
 	
 	private String basePath = "/board/";
+	private TestService testService;
+	
+	public BoardController(TestService testService) {
+		this.testService = testService;
+	}
 	
 	@GetMapping("")
 	public ModelAndView Hello() {
-		return new ModelAndView(this.basePath + "main");
+		ModelAndView view = new ModelAndView(this.basePath + "main");
+		
+		List<User> userList = testService.getUserList();
+		view.addObject("userList", userList);
+		return view;
 	}
 	
 }
